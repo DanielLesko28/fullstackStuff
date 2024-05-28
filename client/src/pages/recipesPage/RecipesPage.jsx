cf8fa19d85d0c7549c802998e68e968bf1e897a2
Daniel Lesko <daniel.lesko@publicisgroupe.cz>
2024-05-28 08:52:26 +0200

    cart component is rendered

--- client/src/pages/recipesPage/RecipesPage.jsx
+++ client/src/pages/recipesPage/RecipesPage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { CurrencyFilter } from '../../redux/currencyFilter/CurenncyFilter';
 import { Inventory } from '../../redux/inventory/Inventory';
 import { useSelector, useDispatch } from 'react-redux';
+import { Cart } from '../../redux/cart/Cart';
 
 const RecipesPage = () => {
     const inventory = useSelector((state) => state.inventory)
     const currencyFilter = useSelector((state) => state.CurrencyFilter)
+    const cart = useSelector((state) => state.cart)
 
     const dispatch = useDispatch()
 
@@ -16,6 +18,7 @@ const RecipesPage = () => {
      <>
      <CurrencyFilter dispatch={dispatch} currencyFilter={currencyFilter} />
      <Inventory inventory={inventory} dispatch={dispatch} />
+     <Cart cart={cart} currencyFilter={currencyFilter} dispatch={dispatch}  />
      </>
     );
 }
